98230346f9c1e76c82948460376dbc29378af94f
Magnus Cochems <magnus@cochems.net>
2025-09-11 10:14:50 +0200

    Final Build

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -1,12 +1,35 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class BackgroundMusic : MonoBehaviour {
+    private static BackgroundMusic Instance;
     [SerializeField] private List<AudioClip> music = new();
+    private int _currentClip;
+    private AudioSource _source;
+    private Action _clipFinished;
     
     private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        
+        _source = GetComponent<AudioSource>();
         DontDestroyOnLoad(gameObject);
+        _clipFinished = () => StartCoroutine(PlayMusic());
+        _clipFinished?.Invoke();
+    }
+
+    private IEnumerator PlayMusic() {
+        _source.PlayOneShot(music[_currentClip]);
+        yield return new WaitForSeconds(music[_currentClip].length);
+        _currentClip++;
+        if (_currentClip == music.Count) _currentClip = 0;
+        _clipFinished?.Invoke();
     }
 }
--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -1,12 +1,33 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class FinishLevel : MonoBehaviour {
+    private VisualElement _endScreen;
+
+    private void Awake() {
+        _endScreen = GetComponent<UIDocument>().rootVisualElement;
+        _endScreen.style.display = DisplayStyle.None;
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.transform.CompareTag("Player")) {
-            Cursor.visible = true;
-            SceneManager.LoadScene(0);
+            StartCoroutine(ShowEndScreen());
         }
+    }
+
+    private IEnumerator ShowEndScreen() {
+        Cursor.visible = true;
+        InputSystem.actions.FindAction("Move").Disable();
+        InputSystem.actions.FindAction("Escape").Disable();
+        InputSystem.actions.FindAction("Change").Disable();
+        InputSystem.actions.FindAction("Jump").Disable();
+        _endScreen.style.display = DisplayStyle.Flex;
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene(0);
     }
 }
